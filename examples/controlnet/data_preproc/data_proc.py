b65753f4bc63fc7cbae5c6666532fa5d35f91d59
mg8527528652 <mayank.gaur@phot.ai>
2024-11-14 18:05:16 +0530

    added data_parquet function

--- examples/controlnet/data_preproc/data_proc.py
+++ examples/controlnet/data_preproc/data_proc.py
@@ -5,7 +5,10 @@ import shutil
 from tqdm import tqdm
 from datasets import load_dataset
 from huggingface_hub import hf_hub_download
-
+from datasets import Dataset, Features
+from datasets import Image as ImageFeature
+from datasets import Value
+import json
 def rename_files(data_folder_path, save_folder_path):
     '''
     inputs:
@@ -46,7 +49,44 @@ def main_rename_files():
 
 
 
+def create_dataset(image_path, condition_image_path, prompt_json_path, save_path):
+    image_names = os.listdir(condition_image_path)
+    print(len(image_names))
+
+    def read_json(json_path):
+        with open(json_path, 'r') as f:
+            return json.load(f)["bg_label"]
+
+    def gen_examples():
+        for i in range(len(image_names)):
+            yield {
+                "image": {"path": image_path + '/' + image_names[i].split(".")[0] + ".jpg"},
+                "condtioning_image": {"path": condition_image_path + '/' + image_names[i]},
+                "text": read_json(prompt_json_path + '/' + image_names[i].split(".")[0] + ".json")
+            }
+
+    train_dataset = Dataset.from_generator(
+        gen_examples,
+        features=Features(
+            image=ImageFeature(),
+            condtioning_image=ImageFeature(),
+            text=Value("string"),
+        ),
+        num_proc=1,
+    )
+    train_dataset.info.features
+    train_dataset.save_to_disk(save_path)
+
+def main_create_dataset():
+    image_path = "/home/ubuntu/projects/python/deepak/controlnet/data/data_after_preproc/input_jpg"
+    condition_image_path = "/home/ubuntu/projects/python/deepak/controlnet/data/data_after_preproc/input_mm_skl_rgbgar"
+    prompt_json_path = "/home/ubuntu/projects/python/deepak/controlnet/data/data_after_preproc/prompt_json"
+    save_path = "/home/ubuntu/projects/python/deepak/controlnet/data/data_after_preproc/train_set_xl_mm_skl_rgbgar"
+    create_dataset(image_path, condition_image_path, prompt_json_path, save_path)
+
+
 
 if __name__ == "__main__":
-    main_rename_files()
-    # get_data_from_hf()
+    # main_rename_files()
+    main_create_dataset() 
+       # get_data_from_hf()
